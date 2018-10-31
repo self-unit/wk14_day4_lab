bf00c104795b082264add3e994a36437f604216a
self-unit <self_unit@hotmail.com>
2018-10-31 14:51:44 +0000

    minor visual tweaks

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -4,7 +4,7 @@ const Cell = ({className}) => {
     return(
         <div>
             <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="50%" cy="50%" r="40" fill={className} />
+            <circle cx="50%" cy="50%" r="40" stroke="black" fill={className} />
         </svg>
         </div>
     )
